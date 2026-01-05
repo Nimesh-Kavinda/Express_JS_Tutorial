efa39d21699b4b400929ca18d30dca1a02adbdca
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2026-01-05 22:37:14 +0530

    Learn about modules

---
+++ app-module.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// application leveel settings
+
+app.set('view engine', 'ejs');
+
+// routing
+
+app.get('/', (req, res) => {
+  res.send('Home Page');
+});
+
+app.post('/api/data', (req, res) => {
+  res.json({ message: 'Data received', data: req.body });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.sendStatus(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
